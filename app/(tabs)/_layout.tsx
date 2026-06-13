909692d79cc2aeda2457a43af8cc6d0df8cedba8
AtarioPZ <victorbty@gmail.com>
2026-06-13 14:36:24 +0530

    logo, routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,7 +46,7 @@ export default function HomeLayout() {
         }}
       />
 
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="requests"
         options={{
           title: "Bookings",
@@ -56,6 +56,23 @@ export default function HomeLayout() {
               size={28}
               color={color}
             />
+          ),
+        }}
+      /> */}
+
+      <Tabs.Screen
+        name="requests"
+        options={{
+          href: null,
+        }}
+      />
+
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="history" size={28} color={color} />
           ),
         }}
       />
